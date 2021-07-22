d11cdc4f1206782e702cc9566578c209df22e7c9
Jorge Nascimento <nascimento.dev@outlook.com>
2021-07-22 13:05:30 -0300

    Exercicio 23

--- exercicio-23/app.js
+++ exercicio-23/app.js
@@ -5,7 +5,9 @@
   - Não modifique a string manualmente.
 */
 
-const myString = '    JS      '
+const myString = "    JS      ";
+
+// console.log(myString.trim());
 
 /*
   02
@@ -17,77 +19,109 @@ const myString = '    JS      '
 */
 
 const people = [
-  { firstName: 'Estevão', lastName: 'Rodriguez', score: 90 },
-  { firstName: 'José', lastName: 'Antônio', score: 100 },
-  { firstName: 'Felipe', lastName: 'Tavares', score: 71 },
-  { firstName: 'Eric', lastName: 'Silva', score: 82 }
-]
+  { firstName: "Estevão", lastName: "Rodriguez", score: 90 },
+  { firstName: "José", lastName: "Antônio", score: 100 },
+  { firstName: "Felipe", lastName: "Tavares", score: 71 },
+  { firstName: "Eric", lastName: "Silva", score: 82 },
+];
+
+const OrdenedByScore = [];
+
+people.forEach((peopleObj) => {
+  OrdenedByScore.push(peopleObj);
+  OrdenedByScore.sort((a, b) => b.score - a.score);
+});
+
+// console.log(OrdenedByScore);
 
 /*
   03
 
   - Gere um novo array com apenas os animais que contém 3 letras no nome;
 
-  Ps: Neste e nos demais exercícios, utilize o debugger para visualizar o valor 
-      final que os exercícios pedem. Após resolver um exercício, remova o 
-      debugger antes de partir para o próximo.
+  Ps: Neste e nos demais exercícios, utilize o debugger para visualizar o valor final que os exercícios pedem. Após resolver um exercício, remova o  debugger antes de partir para o próximo.
 */
 
-const animals = ['cão', 'gato', 'boi', 'leão', 'gnu', 'alce', 'ema']
+const animals = ["cão", "gato", "boi", "leão", "gnu", "alce", "ema"];
+
+const animalWithNameOfthreeLetters = animals.filter(
+  (animal) => animal.length === 3
+);
+
+// console.log(animalWithNameOfthreeLetters);
 
 /*
   04
 
-  - Baseado no array "animals", gere um novo array com a quantidade de letras do 
-    nome de cada animal. Ex.: [6, 8, 2].
+  - Baseado no array "animals", gere um novo array com a quantidade de letras do nome de cada animal. Ex.: [6, 8, 2].
 */
 
+const namesLength = animals.map((animal) => animal.length);
 
+// console.log(namesLength);
 
 /*
   05
 
-  - Através do encadeamento de 2 métodos, gere um novo array com apenas os nomes  
-    dos amigos que moram perto (que contém a propriedade "nearMe" armazenando 
+  - Através do encadeamento de 2 métodos, gere um novo array com apenas os nomes dos amigos que moram perto (que contém a propriedade "nearMe" armazenando 
     true).
 */
 
 const friends = [
-  { id: 1, name: 'João', nearMe: true },
-  { id: 2, name: 'Matheus', nearMe: true },
-  { id: 3, name: 'Luana', nearMe: false },
-  { id: 4, name: 'Nilson', nearMe: true },
-  { id: 5, name: 'Solange', nearMe: false }
-]
+  { id: 1, name: "João", nearMe: true },
+  { id: 2, name: "Matheus", nearMe: true },
+  { id: 3, name: "Luana", nearMe: false },
+  { id: 4, name: "Nilson", nearMe: true },
+  { id: 5, name: "Solange", nearMe: false },
+];
 
+const friendsNearMe = friends
+  .filter((friend) => friend.nearMe)
+  .map((friend) => friend.name);
 
+// console.log(friendsNearMe);
 
 /*
   06
 
-  - Através do encadeamento de 2 métodos, obtenha a soma só dos números ímpares 
-    do array abaixo.
+  - Através do encadeamento de 2 métodos, obtenha a soma só dos números ímpares do array abaixo.
 */
 
-const numbers = [46, 86, 212, 29, 51, 9, 25, 42, 81]
+const numbers = [46, 86, 212, 29, 51, 9, 25, 42, 81];
+
+const sumOddNumbers = numbers
+  .filter((number) => number % 2 !== 0)
+  .reduce((acc, number) => (acc += number), 0);
+
+// console.log(sumOddNumbers);
 
 /*
   07
 
-  - Através do encadeamento de 2 métodos, obtenha a soma da população de todos  
-    países, exceto a China.
+  - Através do encadeamento de 2 métodos, obtenha a soma da população de todos países, exceto a China.
 */
 
-const data = [{
-  country: 'China',
-  population: 1409517397
-}, {
-  country: 'India',
-  population: 1339180127
-}, {
-  country: 'USA',
-  population: 324459463
-}, {
-  country: 'Indonesia',
-  population: 263991379
-}]
+const data = [
+  {
+    country: "China",
+    population: 1409517397,
+  },
+  {
+    country: "India",
+    population: 1339180127,
+  },
+  {
+    country: "USA",
+    population: 324459463,
+  },
+  {
+    country: "Indonesia",
+    population: 263991379,
+  },
+];
+
+const sumPopulation = data
+  .filter((dataCountry) => dataCountry.country !== "China")
+  .reduce((acc, dataCountry) => (acc += dataCountry.population), 0);
+
+// console.log(sumPopulation);
